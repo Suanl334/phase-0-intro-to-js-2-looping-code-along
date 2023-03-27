337fc04da7f779c843c0f3b45cc9ec779120b248
Suan Lian <suanl0002@gmail.com>
2023-03-27 00:33:50 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const names = ["Charlie", "Sam", "John"]
+
+function writeCards(names, event) {
+   return  names.map(name=> `Thank you, ${name} for the wonderful ${event} gift!`)
+}
+
+console.log(writeCards(names, "birthday"))
+
+
+
+function countDown(){
+    for (let count = 10; count >= 0; count--) {
+        console.log(count);
+    }
+}
